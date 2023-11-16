de2076082545ca584c79216205ca8d96d585d91d
mauriValdi <mauricioo.valdi@hotmail.com>
2023-11-16 19:49:39 -0400

    InitialStateForClassSetted
    Set the state of the files for the clean start

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,23 +1,13 @@
-import NavBar from './Navbar';
+import Navbar from './Navbar';
 import Home from './Home';
 
 function App() {
-  const title = 'Welcome to the new blog';
-  const likes = 50;
-  const link = "http://www.google.com"
   return (
     <div className="App">
-      <NavBar></NavBar>
+      <Navbar />
       <div className="content">
-        <Home></Home>
-        <h1>{ title }</h1>
-        <p>{ likes }</p>
-        <p>{ "likes text" }</p>
-        <p>{ [1,2,3,4,5] }</p>
-        <p>{ Math.random() *10 }</p>
-        
-        <a href={link}>Google link</a>
-      </div> 
+        <Home />
+      </div>
     </div>
   );
 }
